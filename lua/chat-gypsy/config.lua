f9a307de9a75471344622557d6ed903bbcc2e81e
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2023-09-13 20:30:46 -0500

    event hooks from opts are registered in config

--- lua/chat-gypsy/config.lua
+++ lua/chat-gypsy/config.lua
@@ -2,6 +2,9 @@ local log_levels = { "trace", "debug", "info", "warn", "error", "fatal" }
 local default_log_level = "warn"
 
 local Config = {}
+
+local events = require("chat-gypsy").events
+
 Config.cfg = {
 	plugin = "gypsy",
 	log_level = default_log_level,
@@ -47,12 +50,12 @@ Config.opts = {
 
 Config.dev = Config.opts.dev_opts
 
--- local event_hooks = function()
--- 	local request = Config.opts.hooks.request
--- 	for hook, _ in pairs(request) do
--- 		events:sub("hook:request:" .. hook, request[hook])
--- 	end
--- end
+local event_hooks = function()
+	local request = Config.opts.hooks.request
+	for hook, _ in pairs(request) do
+		events:sub("hook:request:" .. hook, request[hook])
+	end
+end
 
 Config.init = function(opts)
 	opts = opts or {}
@@ -73,7 +76,7 @@ Config.init = function(opts)
 		end
 	end
 
-	-- event_hooks()
+	event_hooks()
 end
 
 return Config
