7a2fa4170d53a3a7f1bd737fac799b68a6706560
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2023-09-13 19:11:31 -0500

    moves functions and variables to be members of the request class.  creates helper functions to reset chunks, message upon new requests.  user prompts and assistant responses are appending to messages sent to openai

--- lua/chat-gypsy/request.lua
+++ lua/chat-gypsy/request.lua
@@ -3,116 +3,137 @@ local config = require("chat-gypsy.config")
 local cfg = config.cfg
 local opts = config.opts
 
-local raw_chunks = {}
-local chunks = {}
-
 local Request = {}
 Request.__index = Request
 
 function Request.new(log)
 	local self = setmetatable({}, Request)
 	self.log = log
+	self.chunks = {}
+	self.content = ""
+	self.openai_params = cfg.openai_params
+	self.join_content = function()
+		self.content = table.concat(self.chunks, "")
+	end
+	self.on_assistant_response = function()
+		self.content = table.concat(self.chunks, "")
+		self.join_content()
+		self.log.debug("on_user_prompt: " .. self.content)
+		table.insert(self.openai_params.messages, {
+			role = "assistant",
+			content = self.content,
+		})
+	end
+	self.on_user_prompt = function(content)
+		self.content = content
+		self.log.debug("on_user_prompt: " .. self.content)
+		table.insert(self.openai_params.messages, {
+			role = "user",
+			content = self.content,
+		})
+	end
+	self.on_new_request = function()
+		self.chunks = {}
+		self.raw_chunks = {}
+		self.content = ""
+	end
+
+	self.extract_chunk = function(chunk, on_chunk)
+		for line in chunk:gmatch("[^\n]+") do
+			local raw_json = string.gsub(line, "^data: ", "")
+
+			table.insert(self.raw_chunks, raw_json)
+			local ok, path = pcall(vim.json.decode, raw_json)
+			if not ok then
+				goto continue
+			end
+
+			path = path.choices
+			if path == nil then
+				goto continue
+			end
+			path = path[1]
+			if path == nil then
+				goto continue
+			end
+			path = path.delta
+			if path == nil then
+				goto continue
+			end
+			path = path.content
+			if path == nil then
+				goto continue
+			end
+			if #self.chunks == 0 and path == "" then
+				goto continue
+			end
+
+			on_chunk(path)
+			-- events:pub("hook:request:chunk", path)
+			table.insert(self.chunks, path)
+			::continue::
+		end
+	end
+
+	self.exec = function(cmd, args, on_start, on_stdout_chunk, on_complete, on_error)
+		local stdout = vim.loop.new_pipe()
+		local stderr = vim.loop.new_pipe()
+		local stderr_chunks = {}
+		local function on_stdout_read(err, chunk)
+			if err then
+				on_error(err)
+				return
+			end
+			if chunk then
+				vim.schedule(function()
+					on_stdout_chunk(chunk)
+				end)
+			end
+		end
+
+		local function on_stderr_read(err, chunk)
+			if err then
+				on_error(err)
+			end
+			if chunk then
+				table.insert(stderr_chunks, chunk)
+			end
+		end
+
+		on_start()
+
+		local handle, handle_err
+		handle, handle_err = vim.loop.spawn(cmd, {
+			args = args,
+			stdio = { nil, stdout, stderr },
+		}, function(exit_code, _)
+			stdout:close()
+			stderr:close()
+			handle:close()
+
+			vim.schedule(function()
+				if exit_code ~= 0 then
+					on_error(vim.trim(table.concat(stderr_chunks, "")))
+				else
+					on_complete()
+				end
+			end)
+		end)
+
+		if not handle then
+			on_error(handle_err)
+		else
+			stdout:read_start(on_stdout_read)
+			stderr:read_start(on_stderr_read)
+		end
+	end
 	return self
 end
 
-local exec = function(cmd, args, on_start, on_stdout_chunk, on_complete, on_error)
-	local stdout = vim.loop.new_pipe()
-	local stderr = vim.loop.new_pipe()
-	local stderr_chunks = {}
-	local function on_stdout_read(err, chunk)
-		if err then
-			on_error(err)
-			return
-		end
-		if chunk then
-			vim.schedule(function()
-				on_stdout_chunk(chunk)
-			end)
-		end
-	end
-
-	local function on_stderr_read(err, chunk)
-		if err then
-			on_error(err)
-		end
-		if chunk then
-			table.insert(stderr_chunks, chunk)
-		end
-	end
-
-	on_start()
-
-	local handle, handle_err
-	handle, handle_err = vim.loop.spawn(cmd, {
-		args = args,
-		stdio = { nil, stdout, stderr },
-	}, function(exit_code, _)
-		stdout:close()
-		stderr:close()
-		handle:close()
-
-		vim.schedule(function()
-			if exit_code ~= 0 then
-				on_error(vim.trim(table.concat(stderr_chunks, "")))
-			else
-				on_complete()
-			end
-		end)
-	end)
-
-	if not handle then
-		on_error(handle_err)
-	else
-		stdout:read_start(on_stdout_read)
-		stderr:read_start(on_stderr_read)
-	end
-end
-
-local extract_chunk = function(chunk, on_chunk)
-	for line in chunk:gmatch("[^\n]+") do
-		local raw_json = string.gsub(line, "^data: ", "")
-
-		table.insert(raw_chunks, raw_json)
-		local ok, path = pcall(vim.json.decode, raw_json)
-		if not ok then
-			goto continue
-		end
-
-		path = path.choices
-		if path == nil then
-			goto continue
-		end
-		path = path[1]
-		if path == nil then
-			goto continue
-		end
-		path = path.delta
-		if path == nil then
-			goto continue
-		end
-		path = path.content
-		if path == nil then
-			goto continue
-		end
-		if #chunks == 0 and path == "" then
-			goto continue
-		end
-
-		on_chunk(path)
-		-- events:pub("hook:request:chunk", path)
-		table.insert(chunks, path)
-		::continue::
-	end
-end
-
 function Request:query(content, on_response_start, on_response_chunk, on_response_complete)
-	local openai_params = cfg.openai_params
-	table.insert(openai_params.messages, {
-		role = "user",
-		content = content,
-	})
+	self.on_user_prompt(content)
 
-	self.log.debug("query: open_api_params: " .. vim.inspect(openai_params))
+	self.log.debug("query: open_api_params: " .. vim.inspect(self.openai_params))
 	local on_start = function()
 		self.log.debug("query: on_start")
 		-- events:pub("hook:request:start", content)
@@ -120,21 +141,21 @@ function Request:query(content, on_response_start, on_response_chunk, on_respons
 	end
 
 	local on_stdout_chunk = function(chunk)
-		extract_chunk(chunk, on_response_chunk)
+		self.extract_chunk(chunk, on_response_chunk)
 	end
 
 	local on_complete = function()
 		self.log.debug("query: on_complete")
-		on_response_complete(chunks)
+		self.on_assistant_response()
+		on_response_complete(self.chunks)
 	end
 
 	local on_error = function(err)
 		self.log.warn(string.format("query: on_error: %s", err))
 	end
 
-	chunks = {}
-	raw_chunks = {}
-	exec("curl", {
+	self.on_new_request()
+	self.exec("curl", {
 		"--silent",
 		"--show-error",
 		"--no-buffer",
@@ -144,7 +165,7 @@ function Request:query(content, on_response_start, on_response_chunk, on_respons
 		"-H",
 		"Authorization: Bearer " .. opts.openai_key,
 		"-d",
-		vim.json.encode(openai_params),
+		vim.json.encode(self.openai_params),
 	}, on_start, on_stdout_chunk, on_complete, on_error)
 end
 
