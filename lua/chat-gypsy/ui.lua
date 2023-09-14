89583b3ba48889561fbf84ceb16ca14a0d171039
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2023-09-14 05:46:30 -0500

    build_ui accepts a configuration object to return nui layout

--- lua/chat-gypsy/ui.lua
+++ lua/chat-gypsy/ui.lua
@@ -1,11 +1,19 @@
 local nui_pu, nui_lo = require("nui.popup"), require("nui.layout")
 local config = require("chat-gypsy.config")
 local cfg = config.cfg
+local log = require("chat-gypsy").log
 
 local UI = {}
 UI.__index = UI
 
-local function build_ui()
+local function build_ui(layout_config)
+	layout_config = layout_config or {
+		layout = "float", -- left-dock, right-dock
+	}
+	if not vim.tbl_contains({ "float", "left-dock", "right-dock" }, layout_config.layout) then
+		layout_config.layout = "float"
+	end
+
 	local popup_base = {
 		zindex = 50,
 		focusable = true,
@@ -51,29 +59,35 @@ local function build_ui()
 			},
 		},
 	}))
-	local layout = nui_lo(
-		{
-			position = {
-				row = "20%",
-				col = "50%",
-			},
-			size = {
-				width = "70%",
-				height = "70%",
-			},
-			relative = "editor",
-		},
-		nui_lo.Box({
-			nui_lo.Box(chat, {
-				size = "100%",
-			}),
-			nui_lo.Box(prompt, {
-				size = {
-					height = cfg.ui.prompt_height,
+
+	local layout_strategy = function(l_c)
+		if l_c.layout == "float" then
+			return nui_lo(
+				{
+					position = {
+						row = "20%",
+						col = "50%",
+					},
+					size = {
+						width = "70%",
+						height = "70%",
+					},
+					relative = "editor",
 				},
-			}),
-		}, { dir = "col" })
-	)
+				nui_lo.Box({
+					nui_lo.Box(chat, {
+						size = "100%",
+					}),
+					nui_lo.Box(prompt, {
+						size = {
+							height = cfg.ui.prompt_height,
+						},
+					}),
+				}, { dir = "col" })
+			)
+		end
+	end
+	local layout = layout_strategy(layout_config)
 	return {
 		layout = layout,
 		boxes = { chat = chat, prompt = prompt },
@@ -82,7 +96,12 @@ end
 
 function UI.new()
 	local self = setmetatable({}, UI)
-	self.layout = require("chat-gypsy.layout").new(build_ui())
+	local layout_config = {
+		layout = "right-dock",
+		-- layout = "center",
+	}
+	local ui = build_ui(layout_config)
+	self.layout = require("chat-gypsy.layout").new(ui)
 	return self
 end
 
