f46d7e7544750d24f05a63befeaadff26e2df416
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2023-09-23 06:35:18 -0500

    refactors queue to pass in config on .new() to control if queue is ran sync or async

--- lua/chat-gypsy/queue.lua
+++ lua/chat-gypsy/queue.lua
@@ -1,8 +1,11 @@
 FuncQueue = {}
 FuncQueue.__index = FuncQueue
 
-function FuncQueue.new()
+function FuncQueue.new(config)
+	config = config or {}
+	config.sync = config.sync or false
 	local self = setmetatable({}, FuncQueue)
+	self.sync = config.sync or false
 	self.queue = {}
 	self.isRunning = false
 	return self
@@ -14,15 +17,23 @@ function FuncQueue:add(func)
 end
 
 function FuncQueue:executeNext()
-	if not self.isRunning and #self.queue > 0 then
-		self.isRunning = true
+	if self.sync then
+		while #self.queue > 0 do
+			local func = table.remove(self.queue, 1)
+			func()
+		end
+		return
+	else
+		if not self.isRunning and #self.queue > 0 then
+			self.isRunning = true
 
-		local func = table.remove(self.queue, 1)
+			local func = table.remove(self.queue, 1)
 
-		func(function()
-			self.isRunning = false
-			self:executeNext()
-		end)
+			func(function()
+				self.isRunning = false
+				self:executeNext()
+			end)
+		end
 	end
 end
 
