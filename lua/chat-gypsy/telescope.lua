efac39452df24750e6c2eb0a226f93c9c1e9f9c0
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2023-09-27 19:56:47 -0500

    refactors telescope module

--- lua/chat-gypsy/telescope.lua
+++ lua/chat-gypsy/telescope.lua
@@ -9,52 +9,57 @@ local utils = require("chat-gypsy.utils")
 
 local Telescope = {}
 
-local function history_picker(opts)
-	History.get_picker_entries(function(entries)
-		pickers
-			.new(opts, {
-				prompt_title = "History",
-				-- layout_config = {
-				-- 	width = 0.5,
-				-- 	height = 0.5,
-				-- },
-				finder = finders.new_table({
-					results = entries,
-					entry_maker = function(item)
-						return {
-							value = item.entries,
-							display = string.format("%s: %s", item.entries.name, item.entries.description),
-							ordinal = item.entries.name,
-							filename = item.path.full,
-						}
-					end,
-				}),
-				sorter = conf.generic_sorter(),
-				attach_mappings = function(prompt_bufnr, _)
-					actions.select_default:replace(function()
-						actions.close(prompt_bufnr)
-						local selection = action_state.get_selected_entry()
-						vim.print(string.format("history %s selected", selection.value))
-					end)
-					return true
-				end,
-				previewer = previewers.new_buffer_previewer({
-					title = "Chat history",
-					define_preview = function(self, entry, _)
-						vim.api.nvim_buf_set_option(self.state.bufnr, "filetype", "markdown")
-						local contents = utils.decode_json_from_path(entry.filename)
-						for _, message_tbls in pairs(contents.messages) do
-							vim.api.nvim_buf_set_lines(self.state.bufnr, -1, -1, false, { message_tbls.type, "" })
-							for line in message_tbls.message:gmatch("[^\n]+") do
-								vim.api.nvim_buf_set_lines(self.state.bufnr, -1, -1, false, { line })
-							end
-							vim.api.nvim_buf_set_lines(self.state.bufnr, -1, -1, false, { "" })
-						end
-					end,
-				}),
-			})
-			:find()
+local entry_maker = function(item)
+	return {
+		value = item.entries,
+		display = string.format("%s: %s", item.entries.name, item.entries.description),
+		ordinal = item.entries.name,
+		filename = item.path.full,
+	}
+end
+
+local attach_mappings = function(prompt_bufnr, _)
+	actions.select_default:replace(function()
+		actions.close(prompt_bufnr)
+		local selection = action_state.get_selected_entry()
+		log.debug(string.format("history %s selected", vim.inspect(selection)))
 	end)
+	return true
+end
+
+--  TODO: 2023-09-27 - use chat renderer here
+local define_preview = function(self, entry, _)
+	vim.api.nvim_buf_set_option(self.state.bufnr, "filetype", "markdown")
+	local contents = utils.decode_json_from_path(entry.filename)
+	for _, message_tbls in pairs(contents.messages) do
+		vim.api.nvim_buf_set_lines(self.state.bufnr, -1, -1, false, { message_tbls.type, "" })
+		for line in message_tbls.message:gmatch("[^\n]+") do
+			vim.api.nvim_buf_set_lines(self.state.bufnr, -1, -1, false, { line })
+		end
+		vim.api.nvim_buf_set_lines(self.state.bufnr, -1, -1, false, { "" })
+	end
+end
+
+local get_picker_entries = function(entries, opts)
+	pickers
+		.new(opts, {
+			prompt_title = "History",
+			finder = finders.new_table({
+				results = entries,
+				entry_maker = entry_maker,
+			}),
+			sorter = conf.generic_sorter(),
+			attach_mappings = attach_mappings,
+			previewer = previewers.new_buffer_previewer({
+				title = "Chat history",
+				define_preview = define_preview,
+			}),
+		})
+		:find()
+end
+
+local function history_picker(opts)
+	History.get_picker_entries(get_picker_entries, opts)
 end
 
 function Telescope.history(opts)
