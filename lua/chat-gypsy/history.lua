c1570c7c187b06a8a2f8715785b3732736100fc8
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2023-09-29 06:25:09 -0500

    history is saving correctly

--- lua/chat-gypsy/history.lua
+++ lua/chat-gypsy/history.lua
@@ -3,24 +3,37 @@ local Events = require("chat-gypsy").Events
 local Path = require("plenary.path")
 local utils = require("chat-gypsy.utils")
 
---  BUG: 2023-09-28 - initial prompt being sent is not being saved to history
 local History = {}
 History.__index = History
 
 function History:new()
 	setmetatable(self, History)
-	self.current = {}
 	self.data_dir = vim.fn.stdpath("data") .. "/chat-gypsy"
 	self.id_len = 16
 	self.history_id = utils.generate_random_id(self.id_len)
 	self.file = string.format("%s.json", self.history_id)
 	self.json_path = string.format("%s/%s", self.data_dir, self.file)
-	self.queue = require("chat-gypsy.queue"):new()
 	self:init()
 	return self
 end
 
+function History:init_current()
+	self.current = {
+		id = self.history_id,
+		createdAt = os.time(),
+		updatedAt = os.time(),
+		messages = {},
+		openai_params = {},
+		entries = {
+			name = nil,
+			description = nil,
+			keywords = {},
+		},
+	}
+end
+
 function History:init()
+	self:init_current()
 	Path:new(self.data_dir):mkdir()
 	Events.sub("history:reset", function(queue_next)
 		local request = require("chat-gypsy.request"):new()
@@ -34,11 +47,7 @@ function History:init()
 end
 
 function History:save()
-	local save = function(queue_next)
-		Path:new(self.json_path):write(vim.fn.json_encode(self.current), "w")
-		queue_next()
-	end
-	self.queue:add(save)
+	Path:new(self.json_path):write(vim.fn.json_encode(self.current), "w")
 end
 
 function History:reset()
@@ -46,36 +55,31 @@ function History:reset()
 	self.history_id = utils.generate_random_id(self.id_len)
 	self.file = string.format("%s.json", self.history_id)
 	self.json_path = string.format("%s/%s", self.data_dir, self.file)
-	self.current = {}
+	self:init_current()
 end
 
-function History:add_message(message, type, tokens)
+function History:add_message(message, role, tokens)
 	tokens = utils.deepcopy(tokens)
-	if not type then
+	if not role then
 		return
 	end
-	if not vim.tbl_contains({ "prompt", "response" }, type) then
+	if not vim.tbl_contains({ "user", "assistant" }, role) then
 		return
 	end
+	Log.debug(
+		string.format(
+			[[Adding to history: message "%s" of role "%s" with tokens %s]],
+			message,
+			role,
+			vim.inspect(tokens)
+		)
+	)
 	if not self.current.id then
+		self:init_current()
 		-- 	-- Only return the object.  Compose a json object for this chat with the schema: {name: string, description: string, keywords: string[]}.  The description should be limited to 80 characters.  Break compound words in keywords into multiple terms in lowercase.
-		self.current = {
-			id = nil,
-			createdAt = os.time(),
-			updatedAt = os.time(),
-			messages = {},
-			openai_params = {},
-			entries = {
-				name = nil,
-				description = nil,
-				keywords = {},
-			},
-		}
-
-		-- Log.trace(string.format("Creating new chat: %s", vim.inspect(self.current)))
 	end
 	table.insert(self.current.messages, {
-		type = type,
+		role = role,
 		message = message,
 		time = os.time(),
 		tokens = tokens,
@@ -91,19 +95,13 @@ function History:add_message(message, type, tokens)
 end
 
 function History:add_prompt(message, tokens)
-	local type = "prompt"
-	local add_message = function(queue_next)
-		self:add_message(message, type, tokens)
-	end
-	self.queue:add(add_message)
+	local role = "user"
+	self:add_message(message, role, tokens)
 end
 
 function History:add_response(message, tokens)
-	local add_message = function(queue_next)
-		local type = "response"
-		self:add_message(message, type, tokens)
-	end
-	self.queue:add(add_message)
+	local role = "assistant"
+	self:add_message(message, role, tokens)
 end
 
 function History:add_openai_params(openai_params)
