951e0dd2ede57183f7ad1dde9cde69e1c1675b3d
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2023-10-18 05:24:42 -0500

    removes unneeded/unused utility functions

--- lua/chat-gypsy/utils.lua
+++ lua/chat-gypsy/utils.lua
@@ -1,7 +1,3 @@
-local Log = require("chat-gypsy").Log
-local Job = require("plenary.job")
-local Path = require("plenary.path")
-
 Utils = {}
 
 Utils.generate_random_id = function(len)
@@ -26,86 +22,6 @@ Utils.deepcopy = function(orig)
 		copy = orig
 	end
 	return copy
-end
-
-Utils.tbl_to_json_string = function(table, indent_level)
-	if type(table) == "table" then
-		indent_level = indent_level or 1
-		local indent = string.rep("  ", indent_level)
-		local json_str = "{\n"
-		local comma = ""
-		for k, v in pairs(table) do
-			json_str = json_str .. comma .. indent .. '"' .. tostring(k) .. '": '
-			if type(v) == "table" then
-				json_str = json_str .. Utils.tbl_to_json_string(v, indent_level + 1)
-			else
-				json_str = json_str .. vim.json.encode(v)
-			end
-			comma = ",\n"
-		end
-		json_str = json_str .. "\n" .. string.rep("  ", indent_level - 1) .. "}"
-		json_str = string.gsub(json_str, "\\", "")
-		return json_str
-	else
-		local encoded_str = vim.json.encode(table)
-		encoded_str = string.gsub(encoded_str, "\\", "")
-		return encoded_str
-	end
-end
-
-Utils.get_find_cmd = function()
-	if vim.fn.executable("fdfind") == 1 then
-		return {
-			command = "fdfind",
-			args = {
-				"--type",
-				"f",
-				"--color",
-				"never",
-				"--exact-depth",
-				"1",
-				".",
-			},
-		}
-	else
-		Log.warn("No find utility found.  Install fdfind")
-	end
-end
-
-Utils.find_files_in_directory = function(path, on_files_found, on_error)
-	local find = Utils.get_find_cmd()
-	local args = find.args
-	args[#args + 1] = path
-
-	local job = Job:new({
-		command = find.command,
-		args = args,
-		on_exit = vim.schedule_wrap(function(response, exit_code)
-			if exit_code == 0 then
-				on_files_found(response:result())
-			else
-				on_error(string.format("Error: exit_code: %d", exit_code))
-			end
-		end),
-		on_error = vim.schedule_wrap(function(err, data)
-			on_error(string.format("Error: %s.  Command: %s. Args: %s. Data: %s", err, path, find.command, args, data))
-		end),
-	})
-	job:start()
-end
-
-local read_file = function(path)
-	return Path:new(path):read()
-end
-
-Utils.decode_json_from_path = function(file_path, on_error)
-	local file = read_file(file_path)
-	local ok, json = pcall(vim.json.decode, file)
-	if ok and json then
-		return json
-	else
-		on_error(string.format("Error parsing json from path: %s", file_path))
-	end
 end
 
 Utils.string_to_lines_tbl = function(str)
