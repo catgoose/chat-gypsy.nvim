6ca956e8f0e79239f91b2be276e533904e2dd1a0
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2023-11-05 05:53:12 -0600

    Adds annotations for History class

--- lua/chat-gypsy/history.lua
+++ lua/chat-gypsy/history.lua
@@ -1,19 +1,36 @@
 local Log = require("chat-gypsy").Log
 local Utils = require("chat-gypsy.utils")
 
+---@alias Role "user"|"system"|"assistant"
+
+---@class Message
+---@field role string
+---@field content string
+---@field time number
+---@field tokens table
+
+---@class History
+---@field new fun(self: History): History
+---@field add_message fun(self, content: string, role: string, tokens: table)
 local History = {}
 History.__index = History
 
+---@return History
 function History:new()
 	setmetatable(self, History)
 	self.messages = {}
 	return self
 end
 
+---@return nil
 function History:reset()
 	self.messages = {}
 end
 
+---@param content string
+---@param role Role
+---@param tokens table
+---@return nil
 function History:add_message(content, role, tokens)
 	if not role or not Utils.check_roles(role) then
 		return
@@ -36,6 +53,7 @@ function History:add_message(content, role, tokens)
 	Log.trace(string.format("Inserting new message into history: %s", vim.inspect(self.messages[#self.messages])))
 end
 
+---@return Message[]
 function History:get()
 	return Utils.deep_copy(self.messages)
 end
